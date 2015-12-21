77fac5b4cfcebeb84a217e83d39183431b5b5125
猫王子 <sunshiny.neko@gmail.com>
2015-12-21 16:06:12 +0800

    Updated shell script for lsserver

--- misc/lsserver.sh
+++ misc/lsserver.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
 # /etc/init.d/lsserver.sh
-#
 
-# Carry out specific functions when asked to by the system
+### BEGIN INIT INFO
+# Provides:          LightSword
+# Required-Start:    
+# Required-Stop:     
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: LightSword Server
+# Description:       LightSword Secure Socks5 Proxy
+### END INIT INFO
+
 case "$1" in
   start)
     lsserver -m aes-128-cfb -f
