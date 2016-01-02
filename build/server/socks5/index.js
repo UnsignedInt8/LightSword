22ccda9dd797769585ee6234a2ac7c6e5ce5ac7a
猫王子 <sunshiny.neko@gmail.com>
2016-01-02 08:29:31 +0800

    Filter dst addres for local machine

--- build/server/socks5/index.js
+++ build/server/socks5/index.js
@@ -15,12 +15,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, Promi
         step("next", void 0);
     });
 };
+var os = require('os');
 var connectHandler_1 = require('./connectHandler');
 var udpHandler_1 = require('./udpHandler');
 var socks5Constant_1 = require('../../lib/socks5Constant');
 var socks5Helper = require('../../lib/socks5Helper');
+const illegalAddresses = ['127.0.0.1', '::1', '0.0.0.0', '::0', os.hostname()];
 function handleSocks5(client, data, options) {
     let dst = socks5Helper.refineDestination(data);
+    if (illegalAddresses.any(a => a === dst.addr))
+        return true;
     switch (dst.cmd) {
         case socks5Constant_1.REQUEST_CMD.CONNECT:
             connectHandler_1.connect(client, data, dst, options);
